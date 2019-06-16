57634301ad418d74ea44c2c37eee32cb58755906
Richard Lechner <richard@richardlechner.de>
2019-06-16 21:30:41 +0200

    noch ein Bugfix

--- aquarium-deamon/scripts/startDeamon.sh
+++ aquarium-deamon/scripts/startDeamon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/lib/aquarium
-/usr/bin/java -classpath /var/lib/aquarium/aquarium-deamon-0.0.1-SNAPSHOT.jar:/var/lib/aquarium/mysql.jar  aquarium/Aquarium  /var/lib/aquarium/bin/config
+/usr/bin/java -classpath /var/lib/aquarium/aquarium-deamon-0.0.1-SNAPSHOT.jar:/var/lib/aquarium/mysql.jar:/var/lib/aquarium/bmw-1.0.jar aquarium/Aquarium  /var/lib/aquarium/bin/config
